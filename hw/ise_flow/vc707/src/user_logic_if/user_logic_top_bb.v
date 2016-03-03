d04ae0fa82021544c432a2c2120ded66b1c51b0a
Vipin K <vipin2@ntu.edu.sg>
2016-03-04 01:25:55 +0800

    Cleaning up project directories and adding vc707 tests

---
+++ hw/ise_flow/vc707/src/user_logic_if/user_logic_top_bb.v
@@ -0,0 +1,54 @@
+//--------------------------------------------------------------------------------
+// Project    : SWITCH
+// File       : user_logic.v
+// Version    : 0.1
+// Author     : Vipin.K
+//
+// Description: A dummy user logic for testing purpose
+//--------------------------------------------------------------------------------
+
+module user_logic_top(
+    input              i_user_clk,
+	 input              i_pcie_clk,
+    input              i_rst,
+    //reg i/f 
+    input    [31:0]    i_user_data,
+    input    [19:0]    i_user_addr,
+    input              i_user_wr_req,
+    output  [31:0]     o_user_data,
+    output             o_user_rd_ack,
+    input              i_user_rd_req, 
+    //stream i/f 1
+    input              i_pcie_str1_data_valid,
+    output             o_pcie_str1_ack,
+    input    [63:0]    i_pcie_str1_data,
+    output             o_pcie_str1_data_valid,
+    input              i_pcie_str1_ack,
+    output   [63:0]    o_pcie_str1_data,
+    //stream i/f 2       
+    input              i_pcie_str2_data_valid,
+    output             o_pcie_str2_ack,
+    input    [63:0]    i_pcie_str2_data,
+    output             o_pcie_str2_data_valid,
+    input              i_pcie_str2_ack,
+    output   [63:0]    o_pcie_str2_data,
+    //stream i/f 3
+    input              i_pcie_str3_data_valid,
+    output             o_pcie_str3_ack,
+    input    [63:0]    i_pcie_str3_data,
+    output             o_pcie_str3_data_valid,
+    input              i_pcie_str3_ack,
+    output   [63:0]    o_pcie_str3_data,
+    //stream i/f 4
+    input              i_pcie_str4_data_valid,
+    output             o_pcie_str4_ack,
+    input    [63:0]    i_pcie_str4_data,
+    output             o_pcie_str4_data_valid,
+    input              i_pcie_str4_ack,
+    output   [63:0]    o_pcie_str4_data,
+    //interrupt if
+    output             o_intr_req,
+    input              i_intr_ack
+);
+
+endmodule
